867e7baef7d03c3a95cdf4e7a80b803721e2dc65
harshkumarsingh001 <harshkumaraur2018@gmail.com>
2025-07-07 10:00:38 +0530

    modified button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
                             <div className={`${isScrolled ? "bg-gray-700" : "bg-white"} h-0.5 w-0 group-hover:w-full transition-all duration-300`} />
                         </a>
                     ))}
-                    <button className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${isScrolled ? 'text-black' : 'text-white'} transition-all`}>
+                    <button className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${isScrolled ? 'text-black' : 'text-white'} transition-all`}onClick={()=> navigate('/owner')}>
                         Dashboard
                     </button>
                 </div>
@@ -79,6 +79,11 @@ const Navbar = () => {
                 </div>
 
                 {/* Mobile Menu Button */}
+                {user && <UserButton>
+                    <UserButton.MenuItems>
+                        <UserButton.Action label="My Booking" labelIcon={<BookIcon/>} onClick={()=> navigate('/')} />
+                    </UserButton.MenuItems>
+                   </UserButton>}
                 <div className="flex items-center gap-3 md:hidden">
                     <img onClick={()=>setIsMenuOpen(!isMenuOpen)} src={assets.menuIcon} alt="" className={`${isScrolled && "invert"}h-4`}/>
                 </div>
@@ -95,13 +100,13 @@ const Navbar = () => {
                         </a>
                     ))}
 
-                    <button className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all">
+                   {user && <button className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all" onClick={()=> navigate('/owner')}>
                         Dashboard
-                    </button>
+                    </button>}
 
-                    <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
+                   {!user && <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
                         Login
-                    </button>
+                    </button>}
                 </div>
             </nav>
         
